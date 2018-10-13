00b482d8a55a1bd6e341fe4be322f684d8f692e5
simarpal <simerpal.sangha@gmail.com>
2018-10-13 16:36:14 -0400

    Removed friction Left and right move

--- Assets/playerMoves.cs
+++ Assets/playerMoves.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class playerMoves : MonoBehaviour {
 
+	public Rigidbody rigidbody;
+	public float forwardForce = 1000f;
+	public float sideForce = 500f;
+
 	// Use this for initialization
 	void Start () {
 		
+	}	
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		rigidbody.AddForce(0, 0, forwardForce*Time.deltaTime);
+
+		if(Input.GetKey("d")){
+			rigidbody.AddForce(sideForce*Time.deltaTime, 0, 0);
+		}
+		if(Input.GetKey("a")){
+			rigidbody.AddForce(-sideForce*Time.deltaTime, 0, 0);			
+		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
