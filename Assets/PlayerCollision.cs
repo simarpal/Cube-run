511f68f567107f2d4ff967adb5f99aff805f1781
simarpal <simerpal.sangha@gmail.com>
2018-10-15 21:31:41 -0400

    commit 6:
    added script to stop player on hitting Obstacle

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+	public playerMoves  movement;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter( Collision collision){
+		if(collision.collider.tag == "Obstacle"){
+			movement.enabled = false;
+		}
+	}
+}
